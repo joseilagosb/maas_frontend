e5dfbad01ec64673c3eec35421ad391bbfe41ef7
joseilagosb <joseilagosb@gmail.com>
2024-07-08 00:28:17 -0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="relative -top-[10px] font-medium text-3xl text-left md:text-center">Congrats!</h1>
+    <h3 class="text-xl text-left md:text-center">
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h1>Please login before accesing our monitoring service.</h1>
+  </section>
+</template>
+
+<style></style>
